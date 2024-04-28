f14f1e0e5e1ba6b5c829887bf164312c3fbc7988
rheacharles7 <92655087+rheacharles7@users.noreply.github.com>
2024-04-27 22:16:10 -0400

    updated font and changed unit 1 videos to mp4

--- server.py
+++ server.py
@@ -6,36 +6,36 @@ quiz_res = []
 
 lessons= {
     "1": {"lesson_id": "1",
-    "title": "Learn to sign \"Here\" ",
-    "video": "https://www.youtube.com/embed/2dALbgMQWmk?si=Lg7LalBfsJB9BdOU",
+    "title": "Learn to sign \"Here\"",
+    "video": "/static/files/here.mp4",
     "text": "To sign here, open your palms and lay them flat in front of your stomach with palms facing up. Create an outward circular motion with both hands at the same time",
     "next_lesson": "2",
     "prev_lesson": "beg"
     },
     "2": {"lesson_id": "2",
     "title": "Learn to sign \"Turn Left\"",
-    "video": "https://www.youtube.com/embed/NJbkr3BerEU?si=McXm3adcEVxfoakx",
+    "video": "/static/files/left.mp4",
     "text": "To sign \"turn left\", extend your left hand's thumb and pointer finger while keeping the rest of your hand closed. Point your thumb towards you and your pointer finger towards the sky. Then, turn your hand to the left.",
     "next_lesson": "3",
     "prev_lesson": "1"
     },
      "3": {"lesson_id": "3",
     "title": "Learn to sign \"Turn Right\"",
-    "video": "https://www.youtube.com/embed/dxe-_tQXqC0?si=iL5D65tJfuKZ1yfV",
+    "video": "/static/files/right.mp4",
     "text": "To sign \"turn right\", extend your right hand's index and middle finger, twist them together, and point them to the sky. Have your palm facing towards you. Then, move your right hand to the right so your palm faces forward.", 
     "next_lesson": "4",
     "prev_lesson": "2"
     },
      "4": {"lesson_id": "4",
     "title": "Learn to sign \"Behind\"",
-    "video": "https://www.youtube.com/embed/VoetY5cF1Oo?si=m4omJGl_KJk_wUao",
+    "video": "/static/files/behind.mp4",
     "text": "To sign \"behind\", create two fists, one with each hand. Place one fist behind the other, almost like you're hitting the back of one fist with the other.",
     "next_lesson": "5",
     "prev_lesson": "3"
     },
     "5": {"lesson_id": "5",
     "title": "Learn to sign \"Go Forward\"",
-    "video": "https://www.youtube.com/embed/bsYCP5SEHSk?si=ucJd7EDJxQHm6A9Q",
+    "video": "/static/files/proceed.mp4",
     "text": "To sign \"go forward\", on both hands, extend all fingers and keep them together. Keep your palms facing towards you, and move both hands in a forward motion.",
     "next_lesson": "end",
     "prev_lesson": "4"
